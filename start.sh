2fd60da51e022d24c97ee752e64b1f2f23042e07
Kengo Wada <35723446+KengoWada@users.noreply.github.com>
2022-01-05 09:28:57 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,9 +9,9 @@ clear
 sudo apt-get install python3.8-venv python3.8-dev python3-opencv redis-server postgresql nginx postgresql-contrib libpcap-dev libpq-dev -y
 clear
 
-git clone https://github.com/KengoWada/raid_be.git
+git clone https://github.com/KengoWada/raid_be.git /home/raid/raid_be
 
-git clone https://github.com/KengoWada/raid_ml_model.git
+git clone https://github.com/KengoWada/raid_ml_model.git /home/raid/raid_ml_model
 
 mkdir /home/raid/raid_be/static
 mkdir /home/raid/raid_be/media
